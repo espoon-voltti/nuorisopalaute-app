f5063924a67d8bcdf020b09a75958e04ec45ce7c
Kari Riihelä <kari.riihela@reaktor.com>
2019-12-09 08:57:05 +0200

    Basic structure of landing page

--- frontend/src/components/PageLanding.tsx
+++ frontend/src/components/PageLanding.tsx
@@ -30,6 +30,67 @@ const PageLanding: FC = () => {
 					Anna palautetta
 				</button>
 			</div>
+			<div className="jumbotron">
+				<h1>Välitä <br />ja vaikuta</h1>
+				<p>Mikä on sinulle tärkeää?</p>
+				<a className="btn">Anna palautetta</a>
+				<span>tai</span>
+				<a className="btn btn-secondary">Jätä aloite</a>
+			</div>
+			<section className="teaser-container">
+				<div className="teaser teaser--feedback">
+					<h2>Palaute</h2>
+					<p className="teaser-text teaser-text--ingress">Onko lenkkitiellä kaatunut puu? Puuttuuko lähikirjastosta joku kaipaamasi opus? Haluatko kiittää tai antaa meille risuja?</p>
+					<p className="teaser-text">Palaute on nopea ja kevyt keino kertoa meille myös ideoita ja toimenpide-ehdotuksia. Palautetta voit jättää anonyymisti.</p> 
+					<a className="btn btn-secondary">Anna palautetta</a>
+				</div>
+				<div className="teaser teaser--initiative">
+					<h2>Aloite</h2>
+					<p className="teaser-text teaser-text--ingress">Järeämpi keino saada muutosta aikaan.</p>
+					<p className="teaser-text">Onko sinulla joku mainio ehdotus, jonka haluaisit saada eteenpäin ja pelkkä palautteen jättäminen ei tunnu riittävältä ratkaisulta?  Aloite rullaa byrokratian rattaissa hitaasti mutta varmasti eteenpäin.</p> 
+					<a className="btn btn-secondary">Anna palautetta</a>
+				</div>
+			</section>
+				<h2>Mitä palautteilla on saatu aikaan?</h2>
+				<div className="teaser-container">
+					<div className="teaser">
+						<h3>Karaokea kavereiden kanssa!</h3>
+						<img src="" />
+						<p className="teaser-text">Palautteiden innoittamana olemme lisänneet karaoketoimintaa Espoon kirjastoissa.</p>
+					</div>
+					<div className="teaser">
+						<h3>Karaokea kavereiden kanssa!</h3>
+						<img src="" />
+						<p className="teaser-text">Palautteiden innoittamana olemme lisänneet karaoketoimintaa Espoon kirjastoissa.</p>
+					</div>
+					<div className="teaser">
+						<h3>Karaokea kavereiden kanssa!</h3>
+						<img src="" />
+						<p className="teaser-text">Palautteiden innoittamana olemme lisänneet karaoketoimintaa Espoon kirjastoissa.</p>
+					</div>
+					<div className="teaser">
+						<h3>Karaokea kavereiden kanssa!</h3>
+						<img src="" />
+						<p className="teaser-text">Palautteiden innoittamana olemme lisänneet karaoketoimintaa Espoon kirjastoissa.</p>
+					</div>
+					<div className="teaser">
+						<h3>Karaokea kavereiden kanssa!</h3>
+						<img src="" />
+						<p className="teaser-text">Palautteiden innoittamana olemme lisänneet karaoketoimintaa Espoon kirjastoissa.</p>
+					</div>
+				</div>
+			<section>
+				<h2>Nuorten tekemät aloitteet</h2>
+				<dl className="initiative-list">
+					<li>Ruotsinkielen opetuksen muuttaminen   4.12.2019</li>
+					<li>Riistaruokapäiviä kouluihin 23.11.2019</li>
+					<li>Matematiikan maksutonta tukiopetusta 16-19 -vuotiaille, sisältäen lukion! 23.11.2019</li>
+					<li>Pakkoruotsi 23.11.2019</li>
+					<li>Metroihin usb laturi paikat 23.11.2019</li>
+					<li>Helsingin kaupunki voisi tarjota ilmaisia lajittelupisteitä asuinalueille 23.11.2019</li>
+					<li>Pelitietokoneet Leppävaaran nuorisotalolle. 23.11.2019</li>
+				</dl>
+			</section>
 		</div>
 	);
 };
