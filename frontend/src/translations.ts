e5784a236e24457aa77e2fa25ff8ec893889178d
Timo Aho <timo.aho@orangit.fi>
2023-02-24 13:24:10 +0200

    fix: link url to customer service

--- frontend/src/translations.ts
+++ frontend/src/translations.ts
@@ -319,7 +319,7 @@ export const translations = {
 	},
 	urlCustomerService: {
 		"fi-FI":
-			"https://www.espoo.fi/fi-FI/Espoon_kaupunki/Organisaatio_ja_yhteystiedot/Yhteystiedot",
+			"https://www.espoo.fi/fi/espoon-kaupunki/asiakaspalvelu",
 		"sv-FI":
 			"https://www.esbo.fi/sv-FI/Esbo_stad/Organisation_och_kontaktuppgifter/Kontaktuppgifter",
 		"en-FI":
